bb13582dd5f86bdf279df671a9d3e79c35e66f97
James Waples <james@wapl.es>
2020-01-27 10:37:12 +0000

    Run nightly docs in CI

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cargo test --release
 cargo test --release --all-features
 cargo bench --no-run
 
-cargo doc --all-features
+cargo +nightly doc --all-features
 linkchecker target/doc/linuxcnc_hal_sys/index.html
 linkchecker target/doc/linuxcnc_hal/index.html
 

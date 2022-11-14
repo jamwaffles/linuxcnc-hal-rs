c358df948a553d7561f1c03e79248fd26b5d84b3
James Waples <james@wapl.es>
2022-11-14 16:31:16 +0000

    Build LinuxCNC from source

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ set -xe
 cargo clean --doc
 
 cargo fmt --all -- --check
-cargo build
 cargo build --examples
 cargo test --release
 cargo test --release --all-features
 cargo bench --no-run
 
-cargo +nightly doc --all-features
+cargo doc --all-features
 linkchecker target/doc/linuxcnc_hal_sys/index.html
 linkchecker target/doc/linuxcnc_hal/index.html
 linkchecker target/doc/rtapi_logger/index.html
